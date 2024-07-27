15c4f523b415ffab894d478839f4271d9a003c7a
Lucas Rachid Martins <lucasrachid@hotmail.com>
2024-07-26 22:29:49 -0300

    Acesso do usuário

--- authentication/api/serializers.py
+++ authentication/api/serializers.py
@@ -9,9 +9,24 @@ class RoleSerializer(serializers.ModelSerializer):
         
         
 class UsuarioSerializer(serializers.ModelSerializer):
+    roles = serializers.SlugRelatedField(
+        queryset=Role.objects.all(),
+        slug_field='id',
+        many=True
+    )
+
     class Meta:
         model = Usuario
-        fields = '__all__'
+        fields = ['id', 'email', 'nome', 'senha', 'cpf', 'roles']
+        extra_kwargs = {
+            'senha': {'write_only': True},
+        }
+
+    def create(self, validated_data):
+        roles_data = validated_data.pop('roles')
+        usuario = Usuario.objects.create(**validated_data)
+        usuario.roles.set(roles_data)
+        return usuario
         
         
 class UsuarioRoleSerializer(serializers.ModelSerializer):
--- authentication/api/views.py
+++ authentication/api/views.py
@@ -1,12 +1,14 @@
+import bcrypt
 from rest_framework.decorators import action
 from rest_framework import status
 from rest_framework.viewsets import ViewSet
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import status
 from authentication.api.serializers import UsuarioSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from authentication.models import Usuario
 
     
 class AutenticacaoView(ViewSet):
@@ -22,12 +24,34 @@ class AutenticacaoView(ViewSet):
 
     @action(detail=False, methods=['post'], url_path='registro')
     def registro(self, request, *args, **kwargs):
-        print("Teste")
         serializer = UsuarioSerializer(data=request.data)
         if serializer.is_valid():
-            usuario = serializer.save()
+            serializer.save()
             return Response({
                 'usuario': serializer.data,
                 'message': 'User registered successfully'
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    @action(detail=False, methods=['post'], url_path='login')
+    def login(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        senha = request.data.get('senha')
+            
+        try:
+            usuario = Usuario.objects.get(email=email)
+        except Usuario.DoesNotExist:
+            return Response({'error': 'Credenciais inválidas!'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if bcrypt.checkpw(senha.encode('utf-8'), usuario.senha.encode('utf-8')):
+            refresh = RefreshToken.for_user(usuario)
+            serializer = UsuarioSerializer(usuario)
+            return Response({
+                'usuario': serializer.data,
+                'access_token': str(refresh.access_token),
+                'refresh_token': str(refresh)
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Credenciais inválidas!'}, status=status.HTTP_401_UNAUTHORIZED)
+
